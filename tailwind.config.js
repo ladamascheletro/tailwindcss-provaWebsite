78289658d2a9725b70b08baefd7bc1d5ff944294
ladamascheletro <61625484+ladamascheletro@users.noreply.github.com>
2021-02-20 13:52:49 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'delicate_pink': '#f8f0ed',
+    },
+    extend: {}, 
   },
   variants: {
     extend: {},
